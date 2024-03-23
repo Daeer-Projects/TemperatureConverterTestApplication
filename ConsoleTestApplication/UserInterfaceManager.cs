56897fa2b0aa5647397d7024f9662e01974ab7d4
David Clark <krennic51@gmail.com>
2024-03-23 14:09:17 +0000

    Refactored the test app

---
+++ ConsoleTestApplication/UserInterfaceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleTestApplication
+{
+    internal sealed class UserInterfaceManager
+    {
+        internal static void DisplayWelcome()
+        {
+            Console.WriteLine("Hello and welcome to the Temperature Converter test application.");
+            Console.WriteLine("This version is the console application.");
+        }
+
+        internal static void DisplayInformation()
+        {
+            Console.WriteLine("\r\nPlease use 'q', 'Q' or 'quit' to exit.");
+            Console.WriteLine("\r\nPlease enter a value for converting:\r\n");
+        }
+
+        internal static void DisplayResults(
+            string celsiusValue,
+            string fahrenheitValue,
+            string kelvinValue,
+            string gasValue)
+        {
+            Console.WriteLine("\r\nThe results are as follows:");
+            Console.WriteLine($"Celsius: {celsiusValue}\u00b0C");
+            Console.WriteLine($"Fahrenheit: {fahrenheitValue}\u00b0F");
+            Console.WriteLine($"Kelvin: {kelvinValue} kelvin");
+            Console.WriteLine($"Gas mark: {gasValue}");
+        }
+    }
+}
